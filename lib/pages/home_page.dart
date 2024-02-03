f9e801744455f07e11547e4b0229834299781b81
kritan shahi <shahikritan11@gmail.com>
2024-02-03 17:15:47 +0545

    first

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:crudapp/pages/list_student_page.dart';
+import 'package:flutter/material.dart';
+import 'package:crudapp/pages/add_student_page.dart';
+
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text('Flutter FireStore CRUD'),
+            ElevatedButton(
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddStudentPage(),
+                  ),
+                )
+              },
+              child: const Text('Add', style: TextStyle(fontSize: 20.0)),
+              style: ElevatedButton.styleFrom(primary: Colors.deepPurple),
+            )
+          ],
+        ),
+      ),
+      body: const ListStudentPage(),
+    );
+  }
+}
